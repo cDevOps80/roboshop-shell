7d0aa59cfc85e29d20033331c4217d6c575412c6
chaitu1 <chaitu143@gmail.com>
2024-06-23 23:40:10 +0530

    push

--- create-instances/servers_create.sh
+++ create-instances/servers_create.sh
@@ -10,21 +10,19 @@ hostedzone_id="Z0144525QEQQSOE8RRNR"
 domain_name="azcart.online"
 
 create_instance() {
-
-aws ec2 run-instances \ 
-    --region us-east-1 \             
+aws ec2 run-instances \              
     --instance-type $instance_type \    
     --image-id $ami_id  \    
-    --security-group-ids "$sg_id"  \ 
+    --security-group-ids $sg_id  \ 
    --instance-market-options 'MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}' \
 
    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$component}]" "ResourceType=volume,Tags=[{Key=Name,Value=$component}]"
 
-}   
+}
+   
 
 for component in $server
 do
 create_instance
 done
-
 
