0a3a95d23d9ca0c058f2d1bf8b88df8fd1c68bd8
chaitu1 <chaitu143@gmail.com>
2024-06-24 00:43:20 +0530

    ss

--- create-instances/servers_create.sh
+++ create-instances/servers_create.sh
@@ -8,6 +8,7 @@ sg_id="sg-0665a56c7cd09a0e0"
 instance_type="t2.micro"
 hostedzone_id="Z0144525QEQQSOE8RRNR"
 domain_name="azcart.online"
+env="dev"
 
 
 create_instance() {
@@ -20,7 +21,7 @@ private_ip=$(aws ec2 run-instances \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$component}]" "ResourceType=volume,Tags=[{Key=Name,Value=$component}]" \
     --query "Instances[].PrivateIpAddress" --output text )
 
-sed  -e "s/\$component/${component}/" -e "s/\${record_name}/${component}.${domain_name}/" -e "s/\${private_ip}/${private_ip}/" record.json > /tmp/record.json
+sed  -e "s/\$component/${component}/" -e "s/\${record_name}/${component}-${env}.${domain_name}/" -e "s/\${private_ip}/${private_ip}/" record.json > /tmp/record.json
 
 aws route53   change-resource-record-sets \
   --hosted-zone-id $hostedzone_id \
