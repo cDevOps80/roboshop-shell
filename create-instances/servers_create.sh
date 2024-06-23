f7785a18cc66087fc9ea54f2653378709d6ce5b6
chaitu1 <chaitu143@gmail.com>
2024-06-24 00:38:19 +0530

    push

--- create-instances/servers_create.sh
+++ create-instances/servers_create.sh
@@ -11,17 +11,19 @@ domain_name="azcart.online"
 
 
 create_instance() {
-aws ec2 run-instances \
+private_ip=$(aws ec2 run-instances \
     --image-id $ami_id \
     --instance-type $instance_type \
     --security-group-ids $sg_id \
     --instance-market-options 'MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}' \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$component}]" "ResourceType=volume,Tags=[{Key=Name,Value=$component}]" \
-    --query "Instances[].PrivateIpAddress" --output text
+    --query "Instances[].PrivateIpAddress" --output text)
+
+sed  -e "s/\$component/${component}/" -e "s/\${record_name}/${component}.${domain_name}/" -e "s/\${private_ip}/${private_ip}/" record.json > /tmp/record.json
 
 aws route53   change-resource-record-sets \
   --hosted-zone-id $hostedzone_id \
-  --change-batch file://
+  --change-batch file:///tmp/record.json
 }
 
  for component in $server
