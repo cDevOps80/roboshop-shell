e1b17516b42fff9a9d22eaa793a06a4bc72add35
chaitu1 <chaitu143@gmail.com>
2024-06-23 23:17:32 +0530

    create server using aws cli

---
+++ create-instances/servers_create.sh
@@ -0,0 +1,27 @@
+
+server=$1
+
+ami_id="ami-031d574cddc5bb371"
+sg_id="sg-0665a56c7cd09a0e0"
+instance_type="t2.micro"
+hostedzone_id="Z0144525QEQQSOE8RRNR"
+domain_name="azcart.online"
+
+create_instance() {
+
+aws ec2 run-instances \
+   --region us-east-1
+   --image-id $ami_id \ 
+   --instance-type $instance_type \
+   --security-group-ids $sg_id \ 
+   --instance-market-options 'MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}'
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$component}]' 'ResourceType=volume,Tags=[{Key=Name,Value=$component}]"
+
+}   
+
+for component in $server
+do
+create_instance
+done
+
+
