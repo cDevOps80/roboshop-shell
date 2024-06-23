2bb0ddeb743a1458408e49a00f97e36831af2593
chaitu1 <chaitu143@gmail.com>
2024-06-23 23:41:39 +0530

    push

--- create-instances/servers_create.sh
+++ create-instances/servers_create.sh
@@ -11,9 +11,9 @@ domain_name="azcart.online"
 
 create_instance() {
 aws ec2 run-instances \              
-    --instance-type $instance_type \    
-    --image-id $ami_id  \    
-    --security-group-ids $sg_id  \ 
+    --instance-type ${instance_type} \    
+    --image-id ${ami_id}  \    
+    --security-group-ids ${sg_id}  \ 
    --instance-market-options 'MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}' \
 
    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$component}]" "ResourceType=volume,Tags=[{Key=Name,Value=$component}]"
