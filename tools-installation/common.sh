3f4afc0c15246947cabe124ccb3fff52e1266b4b
chaitu1 <chaitu143@gmail.com>
2024-06-24 21:55:17 +0530

    ss

--- tools-installation/common.sh
+++ tools-installation/common.sh
@@ -4,5 +4,5 @@ N="\e[0m"
 LOG_FILE="/tmp/${tool}.log"
 
 status_check(){
-    [ $? -ne 0 ] && echo -e "$R $1 is failed\ncheck more information in $LOG_FILE file$N" && exit 1 || echo -e "$G $1 is success $N"
+    [ $? -ne 0 ] && echo -e "$R $1 is failed\ncheck more information in $LOG_FILE file $N" && exit 1 || echo -e "$G $1 is success $N"
 }
