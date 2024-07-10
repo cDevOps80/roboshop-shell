db0be2dd37c94865b11583388eb27031fa141f04
chaitu1 <chaitu143@gmail.com>
2024-07-10 20:56:43 +0530

    ss

---
+++ aws-cli/run-instance.sh
@@ -0,0 +1,7 @@
+aws ec2 run-instances \
+    --image-id ami-04629cfb3bd2d73f3 \
+    --instance-type t2.micro \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=webserver}]' \
+    --security-group-ids sg-098c66671a7255c8e \
+    --user-data file://my_script.txt \
+    --key-pair bhanu
