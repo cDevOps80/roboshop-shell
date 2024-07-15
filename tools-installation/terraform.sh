419dfa589cf7144c10e654feb36b966ed7252890
chaitu1 <chaitu143@gmail.com>
2024-07-15 20:15:05 +0530

    adding terraform tool

--- tools-installation/terraform.sh
+++ tools-installation/terraform.sh
@@ -1,8 +1,8 @@
 tool="terraform"
 source common.sh
 
-yum install -y yum-utils
-status_check "Installing yum-utils package" &>> $LOG_FILE
+yum install -y yum-utils &>> $LOG_FILE
+status_check "Installing yum-utils package"
 
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo &>> $LOG_FILE
 status_check "Adding terraform repo-file"
